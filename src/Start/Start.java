c008eb7a5c584ac696e9ed30fbc39aa9ff6744cd
Alex <o.ponijay@gmail.com>
2017-12-31 00:48:21 +0200

    JavaLern

--- src/Start/Start.java
+++ src/Start/Start.java
@@ -2,7 +2,21 @@ package Start;
 
 public class Start {
     public static void main(String[] args) {
+//        float pi = 3.14f;
+//        pi = (float) (pi *2 + 2.2);
+//         int a = 12;
+//           a = 2 * a - (20 + 5);
 
-        System.out.print("Hello World");
+//                boolean b = false;
+//                    char c = 't';
+//        System.out.print(c);
+
+//         int userAge = 21;
+
+//         System.out.print(userAge);
+
+            int b = 12;
+            if (b == 12){
+            System.out.println("Hello");
     }
 }
