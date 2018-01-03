1276e1561807eb885230324a4b58618874ac53d0
Alex <o.ponijay@gmail.com>
2018-01-03 21:33:15 +0200

    HTML, CSS files

--- src/Start/Start.java
+++ src/Start/Start.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class Start {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
 
-        int result = scanner.nextInt ();
-      switch (result) {
-          case -1:
-              System.out.print("Result is -1");
-              break;
-          case 0:
-              System.out.print("Result is 0");
-              break;
-          default:
-              System.out.print("Result is unknown!");
-         }
+//         int result = scanner.nextInt();
+//         do {
+//             System.out.print("Message!");
+//             result = scanner.nextInt();
+//         } while (result != -1);
+//         System.out.print("TEst");
+
+        int counter = 0;
+        while (counter <= 1) {
+            System.out.print("5");
+            counter++;
+        }
     }
 }
 
